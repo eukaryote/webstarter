92f133778994069110fe5b13f810cca04514e502
eukaryote <sapientdust+github@gmail.com>
2015-11-22 22:42:53 -0800

    reformat, and enforce semicolon use

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
-    "extends": "standard",
-    "ecmaFeatures": {
-        "blockBindings": true,
-        "forOr": true,
-        "jsx": true
-    },
-    "plugins": [
-        "standard"
-    ]
+  "extends": "standard",
+  "ecmaFeatures": {
+    "blockBindings": true,
+    "forOr": true,
+    "jsx": true
+  },
+  "rules": {
+    "semi": [2, "always"]
+  },
+  "plugins": [
+    "standard"
+  ]
 };
