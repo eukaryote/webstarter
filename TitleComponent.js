8b84b4df9a3b44e14a437ffa81fd1defe1ec422e
eukaryote <sapientdust+github@gmail.com>
2015-11-22 22:43:36 -0800

    reformat

--- TitleComponent.js
+++ TitleComponent.js
@@ -1,10 +1,11 @@
 require('./TitleComponent.css');
+
 var React = require('react');
 
 module.exports = React.createClass({
-    render: function () {
-        return (
-                <h1 className='name'>Hello, {this.props.name}!</h1>
-        );
-    }
+  render: function () {
+    return (
+      <h1 className='name'>Hello, {this.props.name}!</h1>
+    );
+  }
 });
